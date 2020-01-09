e366070bfe05edfac261f6b2a93909a0a5e34629
Jordan Theisen <jordantheisen08@gmail.com>
2020-01-08 20:03:59 -0600

    css site

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 //const request = require("request");
 
 const app = express();
-//app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
