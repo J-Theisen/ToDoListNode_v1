558214be159882ee7759bd5cbdebe4d69b39d3c1
Jordan Theisen <jordantheisen08@gmail.com>
2020-01-14 20:53:52 -0600

    Finished understanding templating vs layouts lesson in section 21.

--- app.js
+++ app.js
@@ -6,37 +6,49 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 var items = ["Buy food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     var today = new Date();
     //var currentDay = today.getDay();
 
     var options = {
-     weekday: "long",
-     day: "numeric",
-     month: "long"   
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     };
 
     var day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 });
 
-app.post("/", function(req, res){
-    var item = req.body.newItem;
-
-    items.push(item);
-
-    res.redirect("/");
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, function(){
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.listen(3000, function() {
     console.log("Server started on port 3000");
-}); 
+});
